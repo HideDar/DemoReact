e3a100b494c1253180c65d07d7d72ec33bed27d9
HideDar <nguyenquaghuy11@gmail.com>
2023-11-20 17:11:37 +0700

    Nesting Component

--- src/components/Mycompoents.js
+++ src/components/Mycompoents.js
@@ -2,48 +2,16 @@
 // function compoent
 
 import React from "react";
+import UserInfor from "./Userinfor";
 
 class MyCompoents extends React.Component {
 
-    state = {
-        name: 'Hoang',
-        address: 'Thai binh',
-        age: '21',
-    };
 
-    handleClick = (event) => {
-        console.log(">>Click me my button");
-        console.log("Random", Math.floor((Math.random() * 100) + 1));
-
-        this.setState({
-            name: 'Nguyen Hoang',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-    handleOnMoverOver(event) {
-        // console.log(event);
-    }
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        alert('me');
-    }
     //JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.age}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input
-                        type="text"
-                        onChange={(event) => this.handleOnChangeInput(event)}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor></UserInfor>
             </div>
         );
     }
