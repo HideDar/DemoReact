b32add56855a63fba677cf001a2d881368e45810
HideDar <nguyenquaghuy11@gmail.com>
2023-12-12 19:39:08 +0700

    bootstrap nav

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyCompoents from './components/Mycompoents';
 import React from 'react';
+import Header from './components/Header/Header';
 
-// class compoents
 
-class App extends React.Component {
+const App = () => {
 
-  render() {
-    return (
-      <div className='app-container'>
-        Hello World
-        <MyCompoents></MyCompoents>
-      </div>
-    );
-  }
+  return (
+    <div className="app-container">
+      <Header></Header>
+    </div>
+  );
 }
-
-
-//Function compoents
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello World
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
